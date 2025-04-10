f8f67fd61199a70e687903e59583497dbd8950c6
ravindrakaya <kayaravindra118@gmail.com>
2025-04-10 09:11:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // 👈 this is the key part
+  base: "/book-collection-manager/", // 👈 this is the key part
 });
